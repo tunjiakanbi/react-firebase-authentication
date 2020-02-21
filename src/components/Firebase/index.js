ba3fd524919f534f8ca422781083d06ccc2dab41
Tunji Akanbi <ac7236yr@e2405-a1520624.century.edu>
2020-02-20 19:18:36 -0600

    set up firebase context

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import FirebaseContext from './context';
+import Firebase from './firebase';
+
 const Firebase = () => (
   <div>
     <h1>Firebase</h1>
   </div>
 );
 export default Firebase;
+export { FirebaseContext };
